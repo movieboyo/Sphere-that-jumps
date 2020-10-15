19401fdc4294ca54e1b8d7275835cb43d74535af
Vlad Cojocariu-Rujoi <cojocariurujoivlad@gmail.com>
2020-10-15 17:55:21 +0300

    running animation

--- a sphere that can jump/Assets/playercontroller2D.cs
+++ a sphere that can jump/Assets/playercontroller2D.cs
@@ -53,10 +53,14 @@ public class playercontroller2D : MonoBehaviour
         if(Input.GetKey("d") || Input.GetKey("right"))
         { 
             rb2d.velocity = new Vector2(runspeed, rb2d.velocity.y);
+            animator.Play("player_run");
+            spriteRenderer.flipX = false;
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {   
             rb2d.velocity = new Vector2(-runspeed, rb2d.velocity.y);
+            animator.Play("player_run");
+            spriteRenderer.flipX = true;
         }
         else 
         {

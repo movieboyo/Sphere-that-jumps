72ce176d1aa22c238c9a4e0bba12dddab7dd4371
Vlad Cojocariu-Rujoi <cojocariurujoivlad@gmail.com>
2020-10-17 20:35:43 +0300

    playerattack sketch

---
+++ a sphere that can jump/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   public int health;
+   public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("damage TAKEN ! ");
+    }
+}
---
+++ a sphere that can jump/Assets/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public transfrom attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+
+    
+
+
+    void Update()
+    {
+        if(timeBtwAttack <= 0 )
+        {
+            if(Input.GetKey(MouseButton.left))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(i = 0; i < enimiesToDamage.Lenght; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+                    startTimeBtwAttack = 0.3;
+                    timeBtwAttack = startTimeBtwAttack;
+    
+            else
+            {
+                timeBtwAttack -= Time.deltaTime;
+            }
+        }
+
+        void OnDrawGizmoSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPos.position, attackRange);
+        }
+
+        
+    }
+}
